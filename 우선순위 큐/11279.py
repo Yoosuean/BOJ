f81eb38bda0e6aaa0803e204e8d306be3a4d426f
Yoosuean <awetumnn@gmail.com>
2022-12-30 23:46:15 +0900

    221230

--- 우선순위 큐/11279.py
+++ 우선순위 큐/11279.py
@@ -1,13 +1,16 @@
 #BOJ 11279 최대 힙
 import sys
+import heapq as hq
 input=sys.stdin.readline
 
 n=int(input())
-arr=[0]
+arr=[]
 for _ in range(n):
     x=int(input())
-    arr.append(x)
-    if x==0:
-        m=max(arr)
-        print(m)
-        arr.remove(m)
+    if x:
+        hq.heappush(arr,(-x,x))
+    else:
+        if len(arr)>=1:
+            print(hq.heappop(arr)[1])
+        else:
+            print(0)
