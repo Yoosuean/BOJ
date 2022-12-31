29f8a728f5278d9a3436c71795169de64eb2dfe6
Yoosuean <awetumnn@gmail.com>
2022-12-31 22:18:26 +0900

    221231

---
+++ 우선순위 큐/1927.py
@@ -0,0 +1,16 @@
+#BOJ 1927 최소 힙
+import sys
+import heapq as hq
+input=sys.stdin.readline
+
+n=int(input())
+arr=[]
+for _ in range(n):
+    x=int(input())
+    if x:
+        hq.heappush(arr,x)
+    else:
+        if len(arr)>=1:
+            print(hq.heappop(arr))
+        else:
+            print(0)
