092ed9048e27a422e33e60b92c2ac1323ccc9af1
Yoosuean <awetumnn@gmail.com>
2023-01-24 22:56:01 +0900

    220124

--- 동적 계획법1/2579.py
+++ 동적 계획법1/2579.py
@@ -1,16 +1,18 @@
-#BOJ 2579 계단 오르기
+#BOJ 2579 계단 오르기 *
 import sys
 input = sys.stdin.readline
-arr=[0]*301
-dp = [0]*301
-n = int(input())
+n=int(input())
+stairs=list(int(input()) for _ in range(n))
+dp=[0]*n
 
-for i in range(n):
-    arr[i]=int(input())
+dp[0]=stairs[0]
+dp[1]=stairs[0]+stairs[1]
+dp[2]=max(stairs[0]+stairs[2],stairs[1]+stairs[2])
 
-dp[0]=arr[0]
-dp[1]=arr[0]+arr[1]
-dp[2]=max(arr[0]+arr[2],arr[1]+arr[2])
 for i in range(3,n):
-    dp[i]=max(dp[i-2] + arr[i] , dp[i-3] + arr[i] + arr[i - 1])
+    dp[i]=max(dp[i-2]+stairs[i],dp[i-3]+stairs[i-1]+stairs[i])
 print(dp[n-1])
+
+# 계단 오르는 방법 두가지
+# 10 > 20 25
+# 10 > 15 25 
---
+++ 브루트 포스/2798.py
@@ -0,0 +1,15 @@
+#BOJ 2798 블랙잭
+n,m=map(int,input().split())
+cards=list(map(int,input().split()))
+res=0
+
+for i in range(0,n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            sum=cards[i]+cards[j]+cards[k]
+            if res<sum<m:
+                res=sum
+            elif sum==m:
+                res=sum
+                break
+print(res)
