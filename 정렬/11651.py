320a6bc04db92a24a2afacf593ad0c77eb5cbf6c
Yoosuean <awetumnn@gmail.com>
2023-02-05 23:02:41 +0900

    230205

--- 정렬/11651.py
+++ 정렬/11651.py
@@ -1,11 +1,11 @@
-#BOJ 11650 좌표 정렬하기
+#BOJ 11651 좌표 정렬하기 2
 import sys
 input=sys.stdin.readline
 n=int(input())
 nums=[]
 for _ in range(n):
     nums.append(list(input().split()))
-nums.sort(key=lambda num: (int(num[0]), int(num[1])))
+nums.sort(key=lambda num: (int(num[1]), int(num[0])))
 # num 값을 문자열로 정렬하면 101보다 11이 크다고 판단, 꼭 정수로 바꿔주기
 for num in nums:
     print(num[0],num[1])
