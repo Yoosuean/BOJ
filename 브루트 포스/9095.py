f6ad2487219e8346b10489ef5a627602ca73b3b6
Yoosuean <awetumnn@gmail.com>
2023-03-09 23:49:12 +0900

    230309

---
+++ 브루트 포스/9095.py
@@ -0,0 +1,11 @@
+#BOJ 3085 사탕게임
+t=int(input())
+dp=[0]*1001
+
+dp[0]=1
+
+for _ in range(t):
+    n=int(input())
+    for i in range(1,n+1):
+        dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+    print(dp[n])
